1d3452b1f87e8b344def1722e76409690eee9315
Please <m.shakeel9038@gmail.com>
2024-12-07 16:22:27 +0530

    Database connection implemented

--- src/api/Connector.php
+++ src/api/Connector.php
@@ -2,6 +2,36 @@
 
 declare(strict_types = 1);
 
-class DatabaseConnector {}
+class DatabaseConnectionDetails {
+    private string $dtbs_name = 'BOOKSTORE';
+    private string $user_name = 'root';
+    private string $domn = 'localhost';
+    private string $pswd = '9051';
+
+    final public function getDatabaseName(): string {
+        return $this->dtbs_name;
+    }
+
+    final public function getUserName(): string {
+        return $this->user_name;
+    }
+
+    final public function getDomain(): string {
+        return $this->domn;
+    }
+
+    final public function getPassword(): string {
+        return $this->pswd;
+    }
+}
+
+$dtbs_cntn_dtls = new DatabaseConnectionDetails();
+
+$dtbs_cntn = new mysqli(
+    $dtbs_cntn_dtls->getDomain(),
+    $dtbs_cntn_dtls->getUserName(),
+    $dtbs_cntn_dtls->getPassword(),
+    $dtbs_cntn_dtls->getDatabaseName(),
+);
 
 ?>
