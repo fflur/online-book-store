5643a593cf50cf4ed5c1047117c421d4fff70755
Please <m.shakeel9038@gmail.com>
2024-12-11 18:38:59 +0530

    implemented switch instead of if-else

--- src/api/book_detail.php
+++ src/api/book_detail.php
@@ -6,20 +6,28 @@ header('Access-Control-Allow-Origin: http://localhost');
 header('Access-Control-Allow-Methods: GET');
 header('Content-Type: application/json; charset=utf-8');
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    header('HTTP/1.1 200 OK');
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        if (!isset($_GET['id'])) {
+            header("HTTP 204 'id' value not found");
+        }
 
-    $rows = $msql_dtbs->query(
-        'SELECT PBLG_DATE, TTLE, ATHR, GNRE,'.
-        'PBLR, CVER_IMGE_UFRL, REVW_SCRE FROM BOOKS WHERE ID = ' .
-        $_GET['id'] .
-        ';'
-    );
+        else {
+            header('HTTP/1.1 200 OK');
+            $rows = $msql_dtbs->query(
+                'SELECT PBLG_DATE, TTLE, ATHR, GNRE,'.
+                'PBLR, CVER_IMGE_UFRL, REVW_SCRE FROM BOOKS WHERE ID = ' .
+                $_GET['id'] .
+                ';'
+            );
+            echo json_encode($rows->fetch_assoc());
+        }
 
-    echo json_encode($rows->fetch_assoc());
+        break;
+
+    default:
+        header('HTTP/1.1 405 Method not allowed');
 }
-
-else header('HTTP/1.1 405 Method not allowed');
 
 header_remove();
 
