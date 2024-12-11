7de38590c3d1742115d981c2109722265eb24e1d
Please <m.shakeel9038@gmail.com>
2024-12-11 19:57:05 +0530

    Added headers

--- src/api/check_user_existence.php
+++ src/api/check_user_existence.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/Connector.php';
 
+header('Access-Control-Allow-Origin: http://localhost');
+header('Access-Control-Allow-Methods: GET');
+header('Content-Type: application/json; charset=utf-8');
+
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     http_response_code(405); // Method Not Allowed
     exit;
