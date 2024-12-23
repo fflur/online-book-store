afaab1601666bd5dbc1190420b4ffc68bba2aa7c
Please <m.shakeel9038@gmail.com>
2024-12-23 22:57:05 +0530

    Added enum BookType

---
+++ src/utils/BookType.php
@@ -0,0 +1,53 @@
+<?php
+
+enum BookType {
+    // Fiction
+    case Classic;
+    case Crime;
+    case Fantasy;
+    case Horror;
+    case HistoricalFiction;
+    case Mystery;
+    case Poetry;
+    case Plays;
+    case ScienceFiction;
+    case ShortStories;
+    case Thrillers;
+    case War;
+
+    // Non Fiction
+    case Autobiography;
+    case Memoir;
+    case Essay;
+    case Academic;
+    case SelfHelp;
+
+    public static function ListCategories(): array {
+        return [
+            'fiction' => [
+                self::Classic,
+                self::Crime,
+                self::Fantasy,
+                self::Horror,
+                self::HistoricalFiction,
+                self::Mystery,
+                self::Poetry,
+                self::Plays,
+                self::ScienceFiction,
+                self::ShortStories,
+                self::Thrillers,
+                self::War,
+            ],
+
+            'non_fiction' => [
+                self::Autobiography,
+                self::Memoir,
+                self::Essay,
+                self::Academic,
+                self::SelfHelp,
+            ],
+        ];
+    }
+}
+
+?>
