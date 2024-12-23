a9b04b63dc2c4472b75605f75028b5b39cb3b26c
Please <m.shakeel9038@gmail.com>
2024-12-23 22:08:16 +0530

    unnecessary class removed

--- src/api/database_connector.php
+++ src/api/database_connector.php
@@ -2,20 +2,11 @@
 
 declare(strict_types = 1);
 
-class DatabaseConnectionDetails {
-    public string $dtbs_name = 'BOOKSTORE';
-    public string $user_name = 'root';
-    public string $domn = 'localhost';
-    public string $pswd = '9051';
-}
+string $dtbs_name = 'BOOKSTORE';
+string $user_name = 'root';
+string $domn = 'localhost';
+string $pswd = '9051';
 
-$dtbs_cntn_dtls = new DatabaseConnectionDetails();
-
-$msql_dtbs = new mysqli(
-    $dtbs_cntn_dtls->domn,
-    $dtbs_cntn_dtls->user_name,
-    $dtbs_cntn_dtls->pswd,
-    $dtbs_cntn_dtls->dtbs_name,
-);
+$msql_dtbs = new mysqli(domn, user_name, pswd, dtbs_name);
 
 ?>
