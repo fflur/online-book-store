88ee5d11520b51aba2b0d682a3e8f15baed4b192
Please <m.shakeel9038@gmail.com>
2024-12-25 20:59:23 +0530

    Dunno what I fixed

--- src/api/customer/register.php
+++ src/api/customer/register.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../database_connector.php';
 require_once __DIR__ . '/customer_procedures.php';
 require_once __DIR__ . '/../../entities/Customer.php';
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
