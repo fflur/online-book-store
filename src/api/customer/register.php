eeba1f9deb7e5786fcc213717e23f63b5ee5e51e
Please <m.shakeel9038@gmail.com>
2024-12-25 21:12:40 +0530

    arranged header instruction

--- src/api/customer/register.php
+++ src/api/customer/register.php
@@ -4,7 +4,6 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-declare(strict_types = 1);
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -20,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 
+declare(strict_types = 1);
 require_once __DIR__ . '/../database_connector.php';
 require_once __DIR__ . '/customer_procedures.php';
 require_once __DIR__ . '/../../entities/Customer.php';
