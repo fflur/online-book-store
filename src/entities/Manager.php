ea94d198390653c613f0dac768a6253f665ffbc1
Please <m.shakeel9038@gmail.com>
2024-12-28 21:11:03 +0530

    Created new entity: Manager

---
+++ src/entities/Manager.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+require_once __DIR__ . '/User.php';
+
+class Manager extends User {
+    private UserType $mber_stts = UserType::Manager;
+
+    public function __construct(
+        string $frst_name,
+        ?string $mdle_name = null,
+        string $last_name,
+        string $user_name,
+        string $mail_addr,
+        string $phne_nmbr,
+        string $stte,
+        string $dsrt,
+        string $strt,
+        string $home_nmbr,
+        ?string $desc = null,
+        UserType $user_type = UserType::Manager,
+    ) {
+        parent::__construct(
+            $frst_name,
+            $mdle_name,
+            $last_name,
+            $user_name,
+            $mail_addr,
+            $phne_nmbr,
+            $stte,
+            $dsrt,
+            $strt,
+            $home_nmbr,
+            $desc
+        );
+    }
+
+    public function GetMemberStatus(): UserType {
+        return $this->mber_stts;
+    }
+}
+?>
