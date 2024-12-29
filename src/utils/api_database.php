ed77963190be297e40f2f81336a43f2b0a9a6835
Please <m.shakeel9038@gmail.com>
2024-12-29 18:30:37 +0530

    Introduced a function for getting the instance of database. No need to create an instance inside the function as the relevant datas needed to initialize the object is in the api_database module

--- src/utils/api_database.php
+++ src/utils/api_database.php
@@ -6,10 +6,9 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $file_path = 'C:\Users\pc\codes\web_development\online_book_store\docs';
 $dotenv = Dotenv\Dotenv::createImmutable($file_path);
 $dotenv->load();
-$dtbs_name = $_ENV['DATABASE_NAME'];
-$user_name = $_ENV['USERNAME'];
-$domn = $_ENV['DOMAIN'];
-$pswd = $_ENV['PASSWORD'];
-$msql_dtbs = new mysqli($domn, $user_name, $pswd, $dtbs_name);
+
+function GetDatabaseInstance(): mysqli {
+    return new mysqli($_ENV['DOMAIN'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DATABASE_NAME']);
+}
 
 ?>
